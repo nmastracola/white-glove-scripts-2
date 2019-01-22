4ae57f7493128d0eb2c8bc7cab4f7ddf305702d6
Nick Mastracola <nmastracola@castor.local>
2019-01-22 16:55:50 -0700

    worked on creation file

--- handlers/create/createAssignments.js
+++ handlers/create/createAssignments.js
@@ -4,8 +4,37 @@ const cli               = require("./cli-questions")
 const axios             = require("axios")
 const style             = require("ansi-styles");
 const config            = require ("../../config/config")
+const fs                = require("fs");
 const throttledQueue    = require('throttled-queue');
 const throttle          = throttledQueue(5, 1000, true);
 const token             = config.token
+const csvInput       = `./logs/assignmentCreation`
 
-    const 
+    const writeAssignments = (answers) =>{
+        
+        prompt(cli.createQuestions)
+        .then(answers =>{
+
+            if(answers.confirm){
+                fs.readdir(csvInput, (err, files) => {
+
+                    files.forEach(file => {
+                        let inputFilePath = `./logs/assignmentCreation/${file}`
+
+                        let domain        = answer.domain
+
+                        csv()
+                        .fromFile(inputFilePath)
+                        .then((assignments =>{
+                            
+                            assignments.forEach(assignment=>{
+                                throttle(function() {
+
+                                })
+                            })
+                        })
+                    })
+                })
+            }
+        })
+    }
